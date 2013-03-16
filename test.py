d3003db5ccb7519623b8406ebb10df3f3c528e49
Michael Stock-Matthews <mstockmatthews@gmail.com>
2013-03-16 01:10:45 -0700

    Add dirty test script

---
+++ test.py
@@ -0,0 +1,6 @@
+execfile('user.py')
+me = User()
+me.add_feed('http://feeds2.feedburner.com/PitchforkLatestNews')
+me.add_feed('http://feeds.feedburner.com/seriouseatsfeaturesvideos?format=xml')
+me.add_feed('http://pandodaily.com.feedsportal.com/c/35141/f/650422/index.rss')
+me.update_stream()

e665edc10bdf558341f12825a234738220c82508
Michael Stock-Matthews <mstockmatthews@gmail.com>
2013-03-16 01:10:13 -0700

    Add repr, robust comparison and content to item

--- item.py
+++ item.py
@@ -7,16 +7,29 @@ class Item(object):
         self.feed_link = feed_link
         self.date = timegm(item_dict['published_parsed'])
         self.title = item_dict['title']
-        self.content = item_dict['content'][0]['value']
         self.link = item_dict['link']
         self.author = item_dict['author']
         self.id = item_dict['id']
         self.new = True
 
+        try:
+            self.content = item_dict['content'][0]['value']
+        except KeyError:
+            self.content = item_dict['summary']
+
+    def __repr__(self):
+        return "Item<%s, %s (%s)>" % (
+            self.id.encode('utf-8'),
+            self.title.encode('utf-8'),
+            self.feed_name.encode('utf-8')
+        )
+
     def __lt__(self, other):
         return self.date > other.date
 
     def __eq__(self, other):
+        if type(other) == type(u''):
+            return self.id == other
         return self.id == other.id
 
     def mark_as_read(self):

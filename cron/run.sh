0cff6b9fea386914788b69a8bc1e613d4d376dd1
Michael Stock-Matthews <mstockmatthews@gmail.com>
2013-03-24 13:39:10 -0700

    Add local cron stuff

--- cron/run.sh
+++ cron/run.sh
@@ -1,2 +1,7 @@
+echo "------------------------\n" >> `dirname $0`/out.log
+
+echo "Loading venv.....\n" >> `dirname $0`/out.log
 source /Users/mstockmatthews/.virtualenvs/rss/bin/activate
+echo "Updating streams.......\n" >> `dirname $0`/out.log
 python `dirname $0`/update.py
+echo "Complete\n" >> `dirname $0`/out.log

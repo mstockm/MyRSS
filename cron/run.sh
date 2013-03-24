b811f6312dd2568407ca29df1c25b86c36e74f43
Michael Stock-Matthews <mstockmatthews@gmail.com>
2013-03-24 13:47:23 -0700

    Remove logfile

--- cron/run.sh
+++ cron/run.sh
@@ -1,7 +1,2 @@
-echo "------------------------\n" >> `dirname $0`/out.log
-
-echo "Loading venv.....\n" >> `dirname $0`/out.log
 source `dirname $0`/activate
-echo "Updating streams.......\n" >> `dirname $0`/out.log
 python `dirname $0`/update.py
-echo "Complete\n" >> `dirname $0`/out.log

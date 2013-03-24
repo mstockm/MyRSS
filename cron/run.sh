08de75eef0e43ac2fa692eb318c1e0f7d93262a7
Michael Stock-Matthews <mstockmatthews@gmail.com>
2013-03-24 13:42:09 -0700

    Generalize cron run

--- cron/run.sh
+++ cron/run.sh
@@ -1,7 +1,7 @@
 echo "------------------------\n" >> `dirname $0`/out.log
 
 echo "Loading venv.....\n" >> `dirname $0`/out.log
-source /Users/mstockmatthews/.virtualenvs/rss/bin/activate
+source `dirname $0`/activate
 echo "Updating streams.......\n" >> `dirname $0`/out.log
 python `dirname $0`/update.py
 echo "Complete\n" >> `dirname $0`/out.log
